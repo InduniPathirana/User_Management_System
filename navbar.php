f4233d29867a95b4b3a4152a35a75aad5249871e
InduniPathirana <142094159+InduniPathirana@users.noreply.github.com>
2024-12-17 15:50:21 +0530

    Navbar page

---
+++ navbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php">User Management</a>
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="add_user.php">Add User</a></li>
+            <li class="nav-item"><a class="nav-link" href="remove_user.php">Remove User</a></li>
+        </ul>
+    </div>
+</nav>
